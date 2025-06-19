0ec4f27894589dbd67f29fd990d604a84151d7d6
mudithasasanka7 <mudithasasanka7@gmail.com>
2025-06-19 20:11:07 +0530

    git commit message example

--- frontend/src/components/DeletedUsers.jsx
+++ frontend/src/components/DeletedUsers.jsx
@@ -12,18 +12,7 @@ export default function DeletedUsers() {
 
   return (
     <div>
-      <h3>Delete<div style={{ maxWidth: "600px", margin: "auto" }}>
-  <h3>Deleted Users</h3>
-  <ul style={{ paddingLeft: "0", listStyle: "none" }}>
-    {deletedUsers.map(user => (
-      <li key={user._id} style={{ padding: "8px 0", borderBottom: "1px solid #ccc" }}>
-        <strong>{user.name}</strong><br />
-        <small>{user.email}</small>
-      </li>
-    ))}
-  </ul>
-</div>
-d Users</h3>
+      <h3>Deleted Users</h3>
       <ul>
         {deletedUsers.map(user => (
           <li key={user._id}>{user.name} - {user.email}</li>
--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -1,4 +1,3 @@
-// UserForm.jsx
 import { useState } from "react";
 import axios from "axios";
 
